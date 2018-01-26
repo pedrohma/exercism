70d84600a8f98e5638164a99fa95097599bd8ce5
Pedro Henrique de Miranda Arthur <pedro@aceelectronix.com>
2018-01-25 16:07:46 -0800

    Finishing triangle exercise

---
+++ triangle/triangle.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+var p = fmt.Println
+
+func getTriangle(l1, l2, l3 int){
+	var triangleType = ""
+	if(l1 == l2 && l1 == l3){
+		triangleType = "Equ"
+	} else {
+		if(l1 == l2 || l1 == l3 || l2 == l3){
+			triangleType = "Iso"
+		} else {
+			triangleType = "Sca"
+		}
+	}
+	
+	p(triangleType)
+}
+
+func main() {
+	getTriangle(5,4,7)
+}
